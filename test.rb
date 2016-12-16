e7e3e8f347ae4be526d3a1dce2e906727710771b
rajanak <rajanakk123@gmail.com>
2016-12-16 18:36:01 +0530

    added combinations of string

--- test.rb
+++ test.rb
@@ -2,5 +2,7 @@ arry=Array.new
 puts "Enter the length of string"
 n = gets.chomp.to_i
 string = (0...n).map { ('a'..'z').to_a[rand(26)] }
-puts string
+1.upto(n).flat_map { |n| string.to_a.combination(n).map(&:join)}.each do |c|
+  puts c
+end
 
