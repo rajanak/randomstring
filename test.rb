1201c7d2a3054e12830d9e7fa9ac4d592329d161
rajanak <rajanakk123@gmail.com>
2016-12-16 18:37:09 +0530

    added permutation of string

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@ puts "Enter the length of string"
 n = gets.chomp.to_i
 string = (0...n).map { ('a'..'z').to_a[rand(26)] }
 1.upto(n).flat_map { |n| string.to_a.combination(n).map(&:join)}.each do |c|
-  puts c
+  arry.push(c.chars.permutation.map &:join )
 end
+puts arry
 
