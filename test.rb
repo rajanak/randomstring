78b8a81208c9c2a648129cb61073ba777b93838c
rajanak <rajanakk123@gmail.com>
2016-12-17 19:13:45 +0530

    modified to get english words

--- test.rb
+++ test.rb
@@ -2,8 +2,15 @@ arry=Array.new
 puts "Enter the length of string"
 n = gets.chomp.to_i
 string = (0...n).map { ('a'..'z').to_a[rand(26)] }
-1.upto(n).flat_map { |n| string.to_a.combination(n).map(&:join)}.each do |c|
-  arry.push(c.chars.permutation.map &:join )
+0.upto(n).flat_map { |n| string.to_a.combination(n).map(&:join)}.each do |c|
+
+	arry.push(c.chars.permutation.map &:join )
 end
 puts arry
+permut = arry.flatten
 
+puts "Dictionary words"
+
+words = File.read("/usr/share/dict/words").split
+result = words & permut
+puts result
